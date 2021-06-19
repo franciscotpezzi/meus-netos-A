2fc841c46e46cda58805ac4a39c491783636f8ff
franciscotpezzi <fthunderpezzi@gmail.com>
2021-06-19 18:56:35 -0300

    complemento 01

--- JS/instrucao.js
+++ JS/instrucao.js
@@ -3,24 +3,24 @@ let data = new Date();
 let hora = data.getHours();
 let min = data.getMinutes();
 
-//hora = 17
+hora = 21
 
 function comprimento(){
     if (hora >= 5 && hora <= 12) {
         msg.innerHTML = `Bom dia! são ${hora}:${min} horas`
-        let audioElement = new Audio('bom dia001.ogg')
+        let audioElement = new Audio('../midias/bom dia002.mp3')
         audioElement.play()
     }if(hora > 12 && hora <= 18) {
         msg.innerHTML = `Boa tarde! são ${hora}:${min} horas`
-        let audioElement = new Audio('../midias/boatarde001.mp3')
+        let audioElement = new Audio('../midias/boatarde002.mp3')
         audioElement.play()
     }if(hora > 18 && hora <= 20) {
         msg.innerHTML = `Bom noite! são ${hora}:${min} horas`
-        let audioElement = new Audio('boanoite001.ogg')
+        let audioElement = new Audio('../midias/boanoite002.mp3')
         audioElement.play()
     }if(hora >20){
         msg.innerHTML = `Bom dia! são ${hora}:${min} horas`
-        let audioElement = new Audio('horadedormir.ogg')
+        let audioElement = new Audio('../midias/horadedormir002.mp3')
         audioElement.play()
     }
 }
@@ -31,6 +31,6 @@ function dino(){
 }
 
 function bear(){
-    let audioElement = new Audio('Bear.mp3')
+    let audioElement = new Audio('../midias/bear02.mp3')
         audioElement.play()
 }
